5f037b537ffff631c896d22f7acf318de8e41a79
Bhupendramouny <bhupendramouny3@gmail.com>
2021-07-14 11:51:55 +0545

    Alert message

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,10 +1,99 @@
 @extends('web.layouts.app')
 
 @section('content')
+    <style>
+        /* The Modal (background) */
+        .w3-modal {
+            display: block; /* Hidden by default */
+            position: fixed; /* Stay in place */
+            z-index: 1; /* Sit on top */
+            padding-top: 20px; /* Location of the box */
+            left: 0;
+            top: 0;
+            width : 100%; /* Full width */
+            height: 100%; /* Full height */
+            overflow: hidden; /* Enable scroll if needed */
+            background-color: rgb(0,0,0); /* Fallback color */
+            background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+        }
+
+        /* Modal Content */
+        .w3-modal-content {
+            background-color: #fefefe;
+            margin: auto;
+            border: 1px solid #888;
+            width: 20%;
+        }
+
+        /* The Close Button */
+        .w3-button {
+            color: #aaaaaa;
+            float: right;
+            font-size: 28px;
+            font-weight: bold;
+        }
+
+        .w3-button:hover,
+        .w3-button:focus {
+            color: #000;
+            text-decoration: none;
+            cursor: pointer;
+        }
+        span:hover{
+            cursor: pointer;
+        }
+
+
+
+        /* Add Animation */
+        @-webkit-keyframes slideIn {
+            from {bottom: -300px; opacity: 0}
+            to {bottom: 0; opacity: 1}
+        }
+
+        @keyframes slideIn {
+            from {bottom: -300px; opacity: 0}
+            to {bottom: 0; opacity: 1}
+        }
+
+        @-webkit-keyframes fadeIn {
+            from {opacity: 0}
+            to {opacity: 1}
+        }
+
+        @keyframes fadeIn {
+            from {opacity: 0}
+            to {opacity: 1}
+        }
+        .pip {
+            display: inline-block;
+            position: relative;
+        }
+        .remove {
+            position: absolute;
+            top: 0;
+            color: red;
+        }
+    </style>
+
+
     <div class="flash-message">
         @foreach (['danger', 'warning', 'success', 'info'] as $msg)
             @if(Session::has('alert-' . $msg))
-                 <p class="alert alert-{{ $msg }}">{{ Session::get('alert-' . $msg) }} <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a></p>
+                <div id="id01" class="w3-modal">
+                    <div class="w3-modal-content">
+                        <div class="w3-container">
+                            <div class="modal-header">
+                                <h2 style="color: #ff682c">{{ $msg }}</h2>
+                                <span class="w3-button w3-display-topright"  onclick="document.getElementById('id01').style.display='none'" >&times;</span>
+                            </div>
+                            <div class="modal-body">
+                                {{ Session::get('alert-' . $msg) }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+{{--                 <p class="alert alert-{{ $msg }}">{{ Session::get('alert-' . $msg) }} <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a></p>--}}
             @endif
         @endforeach
     </div>
