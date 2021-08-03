8059e378257ccc28714e41188baff7bc1997ab71
Aviithapa <abhishekthapa115@gmail.com>
2021-08-03 13:47:46 +0545

    Changes"

--- routes/web/frontend.php
+++ routes/web/frontend.php
@@ -8,6 +8,10 @@ Route::get('/config-cache', function(){
     $exitcode=Artisan::call('config:cache');
     return "Cache Cleared Successfully";
 });
+Route::get('/view-clear', function(){
+    $exitcode=Artisan::call('view:clear');
+    return "View Cleared Successfully";
+});
 Route::get('/storage-link', function(){
     $exitcode=Artisan::call('storage:link');
     return "Storage Linked Successfully";
@@ -16,6 +20,7 @@ Route::get('/migrate', function(){
     $exitcode=Artisan::call('migrate');
     return "Migration Successful Linked Successfully";
 });
+
 Route::group(['namespace' => 'Web'], function () {
 
     /*
