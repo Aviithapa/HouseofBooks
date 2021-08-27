94b73248d551dc30115912395dd95b2b0b6a9bb5
Aviithapa <abhishekthapa115@gmail.com>
2021-08-27 13:21:36 +0545

    Book Pack Design

--- resources/views/web/pages/index.blade.php
+++ resources/views/web/pages/index.blade.php
@@ -201,31 +201,25 @@
                         </div>
                         <div class="bbb_viewed_slider_container book">
                             <div class="owl-carousel owl-theme bbb_viewed_slider">
-                                @foreach($Rakshya as $rakshya)
+                                @foreach($Rakshya as $coursework)
+                                    @if($coursework->status=='active')
                                         <div class="owl-item">
-                                            <div class="bbb_viewed_item discount d-flex flex-column align-items-center justify-content-center text-center"  style="border-radius: 20px; ">
-                                                <a href="{{url("productDetails/".$rakshya->id)}}">
-                                                    <div class="bbb_viewed_image rakshya" style="width: 200px ; height: 230px ;">
-                                                        <img src="{{$rakshya->getImage()}}" oncontextmenu="return false;" alt="{{$rakshya->name}}"></div>
-                                                </a>
-                                                <div class="bbb_viewed_content text-center" style="margin-top: -5px;">
-                                                    <h5 style="font-size:14px !important;font-weight: bold; color: black !important; margin-bottom: 1px !important;line-height: 20px;">{{ str_limit($rakshya->name, 18) }} </h5>
-                                                    <p class="mt-3">
-                                                        <button class="btn btn-primary btn-round-sm btn-sm price">RS {{$rakshya->getDicountedPrice()}}</button>
-                                                            <button onclick="addtoCart({{ $rakshya->id }})"  class="btn btn-primary btn-round-sm btn-sm cart-button" >ADD TO CART</button>
-                                                    </p>
+                                            <div class="bbb_viewed_item discount d-flex flex-column align-items-center justify-content-center text-center" style="border-radius: 20px;background:whitesmoke;">
+                                                <div class="bbb_viewed_image" style="width: 230px !important; height: 230px !important;">
+                                                    <a href="{{url("productDetails/".$coursework->id)}}">
+                                                        <img src="{{$coursework->getImage()}}" oncontextmenu="return false;" alt="{{$coursework->name}}" style="height: 250px;">
+                                                    </a>
+                                                </div>
+                                                <div class="bbb_viewed_content text-center book" style="margin-top: -10px">
+                                                    <h5 style="font-size:14px !important;font-weight: bold; color: black !important; margin-bottom: 1px !important;line-height: 20px;">{{ str_limit($coursework->name, 20) }} </h5>
+                                                    <p class="mt-3"><button class="btn btn-primary btn-round-sm btn-sm price" >RS {{$coursework->getDicountedPrice()}}</button>
+                                                        <button onclick="addtoCart({{ $coursework->id }})"  class="btn btn-primary btn-round-sm btn-sm cart-button">ADD TO CART</button></p>
 
                                                     <!-- <div class="bbb_viewed_name"><a href="#">Alkatel Phone</a></div> -->
                                                 </div>
-                                                <ul class="item_marks">
-{{--                                                    <div class="prod-sticker">--}}
-{{--                                                        <p class="prod-sticker-3" style="background-color: #FF8800 !important;">Rakshya Bandhan Special </p><p class="prod-sticker-4 countdown" data-date="29 Jan 2017, 14:30:00"></p>--}}
-{{--                                                    </div>--}}
-{{--                                                    <li class="item_mark item_discount">-{{$rakshya->discount}} %</li>--}}
-                                                    <li class="item_mark item_new">new</li>
-                                                </ul>
                                             </div>
                                         </div>
+                                    @endif
                                 @endforeach
                             </div>
                         </div>
